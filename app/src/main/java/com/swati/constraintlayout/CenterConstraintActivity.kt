38b4d704d73521c45c8ee34681724f2b6a3d4ea1
swatiomar <swati.omar@aricent.com>
2021-01-19 18:04:19 +0530

    Constraint Layout Part 2

---
+++ app/src/main/java/com/swati/constraintlayout/CenterConstraintActivity.kt
@@ -0,0 +1,11 @@
+package com.swati.constraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CenterConstraintActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_center_constraint)
+    }
+}
---
+++ app/src/main/java/com/swati/constraintlayout/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.swati.constraintlayout
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tv_rp.setOnClickListener(this)
+        tv_bc.setOnClickListener(this)
+        tv_cc.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.tv_rp -> {
+                val intent = Intent(this, RelativePositioningActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.tv_bc -> {
+                //Baseline and Bias demo
+                val intent = Intent(this, BaselineConstraintActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.tv_cc -> {
+                //Center positioning and center constraint demo
+                val intent = Intent(this, CenterConstraintActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+}
